1a1c85db550ec2cc5874e450a46b867f319d55bc
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:10 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/nanoport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/nanoport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/nanoport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU nano is a text editor for Unix-like operating systems' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/nanoport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU nano is a text editor for Unix-like operating systems' )]
   }
 }
